e65faa54087a3285eb61d242247ae213d49d22bd
Stefan Machmeier <stefan.machmeier@outlook.com>
2023-05-30 17:13:06 +0200

    Script for running full training

--- run_models.sh
+++ run_models.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+
 sbatch job_cnn.slurm multiclass_cnn_vgg19 -o multiclass_cnn_vgg19.out
 sbatch job_cnn.slurm multiclass_cnn_xception -o multiclass_cnn_xception.out
 sbatch job_vit.slurm multiclass_vit -o multiclass_vit
+
+sbatch job_cnn.slurm multiclass_cnn_vgg19_header -o multiclass_cnn_vgg19_header.out
+sbatch job_cnn.slurm multiclass_cnn_xception_header -o multiclass_cnn_xception_header.out
+sbatch job_vit.slurm multiclass_vit_header -o multiclass_vit_header.out
+
+sbatch job_cnn.slurm binary_cnn_vgg19 -o binary_cnn_vgg19.out
+sbatch job_cnn.slurm binary_cnn_xception -o binary_cnn_xception.out
+sbatch job_vit.slurm binary_vit -o binary_vit
+
+sbatch job_cnn.slurm binary_cnn_vgg19_header -o binary_cnn_vgg19_header.out
+sbatch job_cnn.slurm binary_cnn_xception_header -o binary_cnn_xception_header.out
+sbatch job_vit.slurm binary_vit_header -o binary_vit_header.out
