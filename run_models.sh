05bf176b24426603cd81b22a4ef8f46ae7a73a2a
Stefan Machmeier <stefan.machmeier@outlook.com>
2023-05-30 17:02:08 +0200

    Update script

---
+++ run_models.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sbtach job_cnn.slurm multiclass_cnn_vgg19 -o multiclass_cnn_vgg19.out
+sbtach job_cnn.slurm multiclass_cnn_xception -o multiclass_cnn_xception.out
+sbatch job_vit.slurm multiclass_vit -o multiclass_vit
