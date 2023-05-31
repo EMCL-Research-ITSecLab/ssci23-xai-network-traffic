c4c5bfc29128c4ae9739a3c0fe6f37b45c8237f8
Stefan Machmeier <stefan.machmeier@outlook.com>
2023-05-31 09:29:49 +0200

    Fix run model

--- run_models.sh
+++ run_models.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-sbatch job_cnn.slurm multiclass_cnn_vgg19 -J multiclass_cnn_vgg19
-sbatch job_cnn.slurm multiclass_cnn_xception -J multiclass_cnn_xception
-sbatch job_vit.slurm multiclass_vit -J multiclass_vit
+sbatch -J multiclass_cnn_vgg19 job_cnn.slurm multiclass_cnn_vgg19 
+sbatch -J multiclass_cnn_xception job_cnn.slurm multiclass_cnn_xception 
+sbatch -J multiclass_vit job_vit.slurm multiclass_vit 
 
-sbatch job_cnn.slurm multiclass_cnn_vgg19_header -J multiclass_cnn_vgg19_header
-sbatch job_cnn.slurm multiclass_cnn_xception_header -J multiclass_cnn_xception_header
-sbatch job_vit.slurm multiclass_vit_header -J multiclass_vit_header
+sbatch -J multiclass_cnn_vgg19_header job_cnn.slurm multiclass_cnn_vgg19_header 
+sbatch -J multiclass_cnn_xception_header job_cnn.slurm multiclass_cnn_xception_header 
+sbatch -J multiclass_vit_header job_vit.slurm multiclass_vit_header
 
-sbatch job_cnn.slurm binary_cnn_vgg19 -J binary_cnn_vgg19
-sbatch job_cnn.slurm binary_cnn_xception -J binary_cnn_xception
-sbatch job_vit.slurm binary_vit -J binary_vit
+sbatch -J binary_cnn_vgg19 job_cnn.slurm binary_cnn_vgg19 
+sbatch -J binary_cnn_xception job_cnn.slurm binary_cnn_xception 
+sbatch -J binary_vit job_vit.slurm binary_vit 
 
-sbatch job_cnn.slurm binary_cnn_vgg19_header -J binary_cnn_vgg19_header
-sbatch job_cnn.slurm binary_cnn_xception_header -J binary_cnn_xception_header
-sbatch job_vit.slurm binary_vit_header -J binary_vit_header
+sbatch -J binary_cnn_vgg19_header job_cnn.slurm binary_cnn_vgg19_header 
+sbatch -J binary_cnn_xception_header job_cnn.slurm binary_cnn_xception_header 
+sbatch -J binary_vit_header job_vit.slurm binary_vit_header 
